0d569aa92d74c2a64584dbaa758e3fece42cc935
Christian Torres <Christian.atm24@gmail.com>
2025-01-02 17:14:25 -0300

    Primer commit

---
+++ useCounter.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+
+export const useCounter = (initialValue = 0) => {
+    const [count, setCount] = useState(initialValue);
+    
+    const increment = (value = 1) => setCount(count + value);
+    const decrement = (value = 1) => setCount(count - value);
+    const reset = () => setCount(0);
+
+    return {
+        count,
+        increment,
+        decrement,
+        reset
+    }
+}
